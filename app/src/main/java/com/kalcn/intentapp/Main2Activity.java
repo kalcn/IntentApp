2d3a489712068095b86e945bde8db9485cd758a3
Kalcn <zoon3312@gmail.com>
2020-06-08 11:25:56 +0800

    作業完成

--- app/src/main/java/com/kalcn/intentapp/Main2Activity.java
+++ app/src/main/java/com/kalcn/intentapp/Main2Activity.java
@@ -3,6 +3,7 @@ package com.kalcn.intentapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,6 +11,20 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        calBMI();
+    }
+
+    private void calBMI() {
+        double height,weight,bmi;
+        Bundle bundle = this.getIntent().getExtras();
+        if(bundle !=null){
+            height = Double.parseDouble(bundle.getString("HEIGHT"));
+            weight = Double.parseDouble(bundle.getString("WEIGHT"));
+            height = height/100.00;
+            bmi =weight/(height*height);
+            TextView result = (TextView)findViewById(R.id.txvBMI);
+            result.setText("BMI = " + Double.toString(bmi));
+        }
     }
 
     public void btnClose(View view) {
--- app/src/main/java/com/kalcn/intentapp/MainActivity.java
+++ app/src/main/java/com/kalcn/intentapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btnBMI);
+        btn.setOnClickListener(listener);
+
     }
 
-    public void btnGo2act(View view){
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
-    }
+    View.OnClickListener listener = new View.OnClickListener() {
+        public void onClick(View view) {
+            EditText height = (EditText)findViewById(R.id.edtHeight);
+            EditText weight = (EditText)findViewById(R.id.edtWeight);
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, Main2Activity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("HEIGHT",height.getText().toString());
+            bundle.putString("WEIGHT",weight.getText().toString());
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
+    };
 }
